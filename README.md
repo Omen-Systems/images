47eb8596e98746c9ffe22f508863424082be34f3
Craftzman7 <zezetta@icloud.com>
2022-02-25 20:24:39 +0100

    Begin working on minecraft docker image

---
+++ README.md
@@ -0,0 +1,3 @@
+# Omen Systems Images
+
+These are used internally by our dev teams for running differnt game servers
---
+++ minecraft/setup_server.sh
@@ -0,0 +1,20 @@
+apk update
+apk add curl jq openjdk17
+cd /server
+
+LATEST_VERSION=`curl https://launchermeta.mojang.com/mc/game/version_manifest.json | jq -r '.latest.release'`
+
+if [ -z "$VANILLA_VERSION" ] || [ "$VANILLA_VERSION" == "latest" ]; then
+  MANIFEST_URL=$(curl https://launchermeta.mojang.com/mc/game/version_manifest.json | jq .versions | jq -r '.[] | select(.id == "'$LATEST_VERSION'") | .url')
+else
+  MANIFEST_URL=$(curl https://launchermeta.mojang.com/mc/game/version_manifest.json | jq .versions | jq -r '.[] | select(.id == "'$VANILLA_VERSION'") | .url')
+fi
+
+DOWNLOAD_URL=`curl $MANIFEST_URL | jq .downloads.server | jq -r '. | .url'`
+
+curl -o server.jar $DOWNLOAD_URL
+jar xf server.jar version.json
+JAVA_VERSION=$(jq '.java_version' version.json)
+rm version.json
+apk delete openjdk17
+apk add openjdk$JAVA_VERSION
